091a37bf3fdd16cbe6337ee373afc1189d916005
Phillip Smith <ps@phillipadsmith.com>
2017-03-11 17:20:38 -0800

    Re-building

--- telegram.js
+++ telegram.js
@@ -1,10 +1,30 @@
 "use strict";
 
+var TelegramBot = require('tgfancy'),
+bot,
+api,
+shim;
+
+
 exports.getApi = function() {
 };
 
 exports.load = function() {
+    var token = exports.config.token;
+    bot = new TelegramBot(token, {
+        // all options to 'tgfancy' MUST be placed under the
+        // 'tgfancy' key, as shown below
+        tgfancy: {
+            polling: "true",
+        },
+    });
+
+    api = shim.createIntegration({
+        sendMessage: sendMessage,
+        commandPrefix: exports.config.commandPrefix
+    });
 };
+
 
 exports.start = function() {
 };
